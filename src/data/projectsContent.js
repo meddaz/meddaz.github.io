33cd98dab6e5a8e8f44377a830827cef461de96c
meddaz <40867923+meddaz@users.noreply.github.com>
2022-04-30 08:31:49 -0700

    Added a new listing

--- src/data/projectsContent.js
+++ src/data/projectsContent.js
@@ -261,5 +261,12 @@ export const ProjectsContent = [
       name: "Background Color Picker",
       excerpt: `Change the background color of the page in real time with the color picker`,
       tools: ""
+    },
+   {
+      category: "works",
+      link: "https://github.com/meddaz/optionpicker",
+      name: "Option Picker",
+      excerpt: `Put a few options in the text box (separated by commas), press go, and get help choosing.`,
+      tools: ""
     }
     ];
