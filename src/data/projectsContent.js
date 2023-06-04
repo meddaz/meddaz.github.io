2887463d612e15b6a6e1688e9305899ea03702dc
meddaz <40867923+meddaz@users.noreply.github.com>
2023-06-03 18:08:34 -0700

    Update projectsContent.js
    Changed some of the links

--- src/data/projectsContent.js
+++ src/data/projectsContent.js
@@ -1,13 +1,5 @@
-
 export const ProjectsContent = [
-  {
-    
-      category: "works",
-      link: "https://github.com/meddaz/nftpreviewcard",
-      name: "NFT Preview Card",
-      excerpt: `A challenge from Frontend Mentor`,
-      tools: ""
-    },
+   
     {
       category: "works",
       link: `${window.location.origin}`,
@@ -18,49 +10,49 @@ export const ProjectsContent = [
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/nytTopStories",
+      link: "https://meddaz.github.io/nytTopStories/",
       name: "New York Times Top Stories",
       excerpt: `Display of the New York Times Top Stories, using the official API`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/Movie-Quotes",
+      link: "https://meddaz.github.io/Movie-Quotes/",
       name: "Random Movie Quotes",
       excerpt: `Random movie quotes. Get a new quote with a click of the button.`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/Dog-Gifs",
+      link: "https://meddaz.github.io/Dog-Gifs/",
       name: "Dog GIFs",
       excerpt: `Dogs and more dogs, powered by the Giphy API`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/linkshortener",
+      link: "https://meddaz.github.io/linkshortener/",
       name: "Link Shortener",
       excerpt: `A link shortener using the Bitly API`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/nytbestsellers",
+      link: "https://meddaz.github.io/nytbestsellers/",
       name: "New York Times Bestsellers",
       excerpt: `New York Times Bestseller Lists, updated weekly`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/Magic-8-Ball",
+      link: "https://meddaz.github.io/Magic-8-Ball/",
       name: "Magic 8 Ball",
       excerpt: `Ask a question, get an answer`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/Random-Recipes",
+      link: "https://meddaz.github.io/Random-Recipes/",
       name: "Random Recipes",
       excerpt: `
      Random recipes powered by the MealDB API`,
@@ -68,125 +60,125 @@ export const ProjectsContent = [
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/Anagram-Checker",
+      link: "https://meddaz.github.io/Anagram-Checker/",
       name: "Anagram Checker",
       excerpt: `Compare two words and see if they're anagrams    `,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/Temperature-Converter",
+      link: "https://meddaz.github.io/Temperature-Converter/",
       name: "Temperature Converter",
       excerpt: `Convert from Fahrenheit to Celsius and vice versa.`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/SpaceflightNews",
+      link: "https://meddaz.github.io/SpaceflightNews/",
       name: "Spaceflight News",
       excerpt: `Recent news in spaceflight`,
       tools: ""
     }, 
     {
       category: "works",
-      link: "https://github.com/meddaz/boredapi",
+      link: "meddaz.github.io/boredapi",
       name: "Bored.com Suggestions ",
       excerpt: ` Suggestions on what to do when you're bored, from the Bored.com API`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/randomcocktails",
+      link: "https://meddaz.github.io/randomcocktails",
       name: "Random Cocktails",
       excerpt: `Random cocktail suggestions using the CocktailDB API`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/vowelcounter",
+      link: "meddaz.github.io/vowelcounter",
       name: "Vowel Counter",
       excerpt: `Count the vowels in a word`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/dayoftheweek",
+      link: "meddaz.github.io/dayoftheweek",
       name: "Day of the Week",
       excerpt: `A quick page that shows what day of the week it is`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/catpictures",
+      link: "meddaz.github.io/catpictures",
       name: "Cat Pictures",
       excerpt: `Random cats using the Random.Cat API`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/advice",
+      link: "meddaz.github.io/advice",
       name: "Advice Generator",
       excerpt: `Advice using the Advice Slip API `,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/dogpictures",
+      link: "meddaz.github.io/dogpictures",
       name: "Dog Pictures",
       excerpt: `A random dog pic on refresh`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/RandomUselessFacts",
+      link: "meddaz.github.io/RandomUselessFacts",
       name: "Random Useless Facts",
       excerpt: `Powered by the Random Useless Facts API`,
       tools: ""},
     {
       category: "works",
-      link: "https://github.com/meddaz/PasswordToggle",
+      link: "meddaz.github.io/PasswordToggle",
       name: "Show/Hide Password",
       excerpt: `Show or hide the entered password`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/popularmemes",
+      link: "meddaz.github.io/popularmemes",
       name: "Popular Memes    ",
       excerpt: `Templates for 15 popular memes from the ImgFlip API`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/drawing",
+      link: "meddaz.github.io/drawing",
       name: "Drawing",
       excerpt: `A simple Canvas drawing app`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/nytmostpopular",
+      link: "meddaz.github.io/nytmostpopular",
       name: "New York Times Most Popular Stories",
       excerpt: `A list of the New York Times Most Popular Articles, using the official API`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/counter",
+      link: "meddaz.github.io/counter",
       name: "Counter",
       excerpt: `Increase or decrease the number displayed`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/Calculator",
+      link: "meddaz.github.io/Calculator",
       name: "Calculator",
       excerpt: `A simple calculator`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/greeting",
+      link: "meddaz.github.io/greeting",
       name: "Greeting",
       excerpt: `Type in your name, get a greeting`,
       
@@ -194,79 +186,93 @@ export const ProjectsContent = [
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/dadjokes",
+      link: "meddaz.github.io/dadjokes",
       name: "Dad Jokes",
       excerpt: `Random dad jokes from the I Can Haz Dad Joke? API`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/Dice-Roll",
+      link: "meddaz.github.io/Dice-Roll",
       name: "Dice Roll",
       excerpt: `Roll a pair of dice on button click`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/Random-Background-Color",
+      link: "meddaz.github.io/Random-Background-Color",
       name: "Random Background Color",
       excerpt: `Change the background color on button click`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/wordscramble",
+      link: "meddaz.github.io/wordscramble",
       name: "Word Scramble",
       excerpt: `Unscramble the displayed word`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/bookfinder",
+      link: "meddaz.github.io/bookfinder",
       name: "Book Finder",
       excerpt: `Find books using the Google Books API`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/randomjokes",
+      link: "meddaz.github.io/randomjokes",
       name: "Programming Jokes",
       excerpt: `Programming jokes`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/WhatsMyIP",
+      link: "meddaz.github.io/WhatsMyIP",
       name: "What's My IP?",
       excerpt: `Display your IP address`,
       tools: ""
     },
      {
       category: "works",
-      link: "https://github.com/meddaz/Prime-numbers",
+      link: "meddaz.github.io/Prime-numbers",
       name: "Prime Numbers",
       excerpt: `Check whether or not a number is prime`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/random-number-generator",
+      link: "meddaz.github.io/random-number-generator",
       name: "Random Number Generator",
       excerpt: `Generate random number on button click`,
       tools: ""
     },
     {
       category: "works",
-      link: "https://github.com/meddaz/backgroundcolorpicker",
+      link: "meddaz.github.io/backgroundcolorpicker",
       name: "Background Color Picker",
       excerpt: `Change the background color of the page in real time with the color picker`,
       tools: ""
     },
    {
       category: "works",
-      link: "https://github.com/meddaz/optionpicker",
+      link: "meddaz.github.io/optionpicker",
       name: "Option Picker",
       excerpt: `Put a few options in the text box (separated by commas), press go, and get help choosing.`,
       tools: ""
-    }
+    },
+    {
+      category: "works",
+      link: "meddaz.github.io/Calendar",
+      name: "Calendar",
+      excerpt: `Simple calendar`,
+      tools: ""
+    },
+    {
+      category: "works",
+      link: "meddaz.github.io/typing",
+      name: "Typing",
+      excerpt: `Type as many of the given words as you can in 30 seconds.`,
+      
+  
     ];
